c5d5b6df2a3022d3736ddf1335e055be1ac7195c
Shreyash Singhai <145225995+SHREYASHSINGHAI@users.noreply.github.com>
2026-08-08 18:11:31 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int target = 1;
+        for(int i =0; i< nums.size();i++){
+            if(nums[i]<=0){
+                continue;
+            }
+            else if(nums[i]==target){
+                target++;
+            }
+            else if(nums[i]>target){
+                return target;
+            }
+        }
+        return target;
+    }
+};
