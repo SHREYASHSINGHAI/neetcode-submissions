3028dc1e3ca954fcceed9c72b5e1f0a1de6b3d17
Shreyash Singhai <145225995+SHREYASHSINGHAI@users.noreply.github.com>
2026-08-10 00:57:32 +0530

    Add: find-k-closest-elements - submission-1

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int> diff;
+        vector<int>output;
+        int n = arr.size();
+        // finding difference btwn ele and x
+        for(int i =0; i < n; i++){
+            int dif = abs(x - arr[i]);
+            diff.push_back(dif);
+        }
+        while(k>0){
+            int min = INT_MAX;
+            int max = min;
+            // getting min difference
+            for(int j =0; j <n; j++){
+                if(diff[j] < min){
+                    min = diff[j];
+                }
+            }
+            bool found = false;
+            int min_idx;
+            // getting closest element
+            for(int k = 0; k <n; k++){
+                if(diff[k] == min){
+                    min_idx = k;
+                    output.push_back(arr[min_idx]);
+                    found = true;
+                    diff[k]=max;
+                    break;
+                }
+            }
+            if(found == true){
+                k--;
+                found = false;
+            }
+        }
+        sort(output.begin(), output.end());
+        return output;
+
+    }
+};
