b9a8076711bdd2667d8969d89711af7b25a54050
Shreyash Singhai <145225995+SHREYASHSINGHAI@users.noreply.github.com>
2026-08-19 15:39:48 +0530

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int freq = 0;
+        for(int i = 0; i<nums.size();i++){
+            if(freq == 0){
+                ans = nums[i];
+            }
+            if(nums[i]==ans){
+                freq++;
+            }
+            else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
